bdec3352d21ec5b630e492eea6ef2bff3404ab32
Pat <Pat.Spam@hotmail.de>
2020-02-01 02:19:25 +0100

    AnimationController

---
+++ jam/Assets/ThomasTest/CharacterTestScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTestScript : MonoBehaviour
+{
+    private bool IsPickedUp = false;
+
+    private void Test()
+    {
+        Animator animator = GetComponent<Animator>();
+        if (IsPickedUp)
+        {
+            IsPickedUp = false;
+            animator.SetTrigger("PutDown");
+        }
+        else
+        {
+            IsPickedUp = true;
+            animator.SetTrigger("PickUp");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Test();
+        }
+    }
+}
